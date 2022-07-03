86bbdd0bf5d51a2e0293bc758dec96c92d9b57ec
Elinnoel Nunez <elinnoelnunez@outlook.com>
2022-07-03 16:22:15 -0500

    Clearer comments on heapsort

--- DS Templates/Sorting Algorithms/heapSort.cpp
+++ DS Templates/Sorting Algorithms/heapSort.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void minheapify(int arr[], int n, int i)
+void maxheapify(int arr[], int n, int i)
 {
     int largest = i;
     int left = 2 * i + 1;
@@ -22,11 +22,11 @@ void minheapify(int arr[], int n, int i)
     {
         swap(arr[i], arr[largest]);
 
-        minheapify(arr, n, largest);
+        maxheapify(arr, n, largest);
     }
 }
 
-void maxheapify(int arr[], int n, int i)
+void minheapify(int arr[], int n, int i)
 {
     int smallest = i;
     int left = 2 * i + 1;
@@ -46,27 +46,13 @@ void maxheapify(int arr[], int n, int i)
     {
         swap(arr[i], arr[smallest]);
 
-        maxheapify(arr, n, smallest);
+        minheapify(arr, n, smallest);
     }
 }
 
 void minHeapSort(int arr[], int n)
 {
-    for (int i = n / 2 - 1; i >= 0; i--)
-    {
-        minheapify(arr, n, i);
-    }
-
-    for (int i = n - 1; i > 0; i--)
-    {
-        swap(arr[0], arr[i]);
-
-        minheapify(arr, i, 0);
-    }
-}
-
-void maxHeapSort(int arr[], int n)
-{
+    // Builds a max heap
     for (int i = n / 2 - 1; i >= 0; i--)
     {
         maxheapify(arr, n, i);
@@ -80,16 +66,32 @@ void maxHeapSort(int arr[], int n)
     }
 }
 
+void maxHeapSort(int arr[], int n)
+{
+    // Builds a min heap
+    for (int i = n / 2 - 1; i >= 0; i--)
+    {
+        minheapify(arr, n, i);
+    }
+
+    for (int i = n - 1; i > 0; i--)
+    {
+        swap(arr[0], arr[i]);
+
+        minheapify(arr, i, 0);
+    }
+}
+
 void maxDeleteElt(int arr[], int &size) {
     swap(arr[0], arr[size-1]);
     size--;
-    minheapify(arr, size, 0);
+    maxheapify(arr, size, 0);
 }
 
 void minDeleteElt(int arr[], int &size) {
     swap(arr[0], arr[size-1]);
     size--;
-    maxheapify(arr, size, 0);
+    minheapify(arr, size, 0);
 }
 
 void display(int arr[], int n)
@@ -112,17 +114,17 @@ int main()
     cout << "Unsorted Array: ";
     display(arr, n);
 
-    maxHeapSort(arr, n);
-    minHeapSort(arr2, n2);
+    maxHeapSort(arr, n); // builds min heap and sorts by descending order
+    minHeapSort(arr2, n2); // builds max heap and sorts by ascending order
 
     cout << "Sorted:" << endl;
 
-    cout << "Max Heap: ";
+    cout << "Sorted Heap Descending: ";
     display(arr, n); // 67 65 54 23 13 13 12 11 7 6 5 2
     maxDeleteElt(arr, n);
     display(arr, n); // 65 23 54 11 13 13 12 2 7 6 5 
 
-    cout << "Min Heap: ";
+    cout << "Sorted Heap Ascending: ";
     display(arr2, n2); // 2 5 6 7 11 12 13 13 23 54 65 67
     minDeleteElt(arr2, n2);
     display(arr2, n2); // 5 7 6 13 11 12 13 67 23 54 65
